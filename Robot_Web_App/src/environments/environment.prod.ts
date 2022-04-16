7480888a14bef5134d172edf24c34216b4a58c9c
Gabriel-Hanich <gabriel.hanich1@gmail.com>
2022-04-16 22:16:31 +1000

    Edited dependencies

--- Robot_Web_App/src/environments/environment.prod.ts
+++ Robot_Web_App/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  defaultURL: "http://localhost:3000",
+  defaultURL: "http://localhost:4000/robot_info",
   defaultPollingRate: 1000,
   matchTime: 135,
   autoList: ["Auto_2_Ball_A", "Auto_2_Ball_B", "Auto_3_Ball", "Auto_Do_Nothing"],
