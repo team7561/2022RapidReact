0793aa5bb6709aa099ab42839d11282e315016ec
Gabriel-Hanich <gabriel.hanich1@gmail.com>
2022-04-21 11:00:46 +1000

    Removed all material icons, improved smaller screen support

--- Robot_Web_App/src/app/components/offsets/offsets.component.ts
+++ Robot_Web_App/src/app/components/offsets/offsets.component.ts
@@ -19,12 +19,12 @@ export class OffsetsComponent implements OnInit {
   constructor(private globalVars: DynamicGlobalsService, private robotData: GetRobotDataService) { }
 
   ngOnInit(): void {
-    this.offsetValA = parseFloat(parseFloat(this.globalVars.getVar("A_Offset_Angle")).toFixed(2));
-    this.offsetValB = parseFloat(parseFloat(this.globalVars.getVar("B_Offset_Angle")).toFixed(2));
-    this.offsetValC = parseFloat(parseFloat(this.globalVars.getVar("C_Offset_Angle")).toFixed(2));
-    this.offsetValD = parseFloat(parseFloat(this.globalVars.getVar("D_Offset_Angle")).toFixed(2));
-    
+    this.setOffsets();
     this.globalSub = this.globalVars.getSubject().subscribe(()=>{
+      if(this.offsetValA?.toString() == "NaN"){
+        this.setOffsets();
+      }
+
       if(document.getElementById("iconA")){ // Ensure the element is rendered before transforming them all
         (document.getElementById("iconA") as HTMLElement).style.transform = "rotate(" + this.globalVars.getVar("A_Angle") + "deg)";
         (document.getElementById("iconB") as HTMLElement).style.transform = "rotate(" + this.globalVars.getVar("B_Angle") + "deg)";
@@ -40,12 +40,21 @@ export class OffsetsComponent implements OnInit {
     }
   }
 
+  setOffsets(): void{
+    this.offsetValA = parseFloat(parseFloat(this.globalVars.getVar("A_Offset_Angle")).toFixed(2));
+    this.offsetValB = parseFloat(parseFloat(this.globalVars.getVar("B_Offset_Angle")).toFixed(2));
+    this.offsetValC = parseFloat(parseFloat(this.globalVars.getVar("C_Offset_Angle")).toFixed(2));
+    this.offsetValD = parseFloat(parseFloat(this.globalVars.getVar("D_Offset_Angle")).toFixed(2));
+  }
+
   updateOffsets():void{
     this.robotData.sendRobotData("A_Offset_Angle", this.offsetValA?.toString() as string); // Send the new offsets to the robot
     this.robotData.sendRobotData("B_Offset_Angle", this.offsetValB?.toString() as string);
     this.robotData.sendRobotData("C_Offset_Angle", this.offsetValC?.toString() as string);
     this.robotData.sendRobotData("D_Offset_Angle", this.offsetValD?.toString() as string);
   }
+
+
 
   
 
